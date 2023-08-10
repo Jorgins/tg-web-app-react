02f961e21683534046f5c6276fb85a6241e4f2c7
Georgii <geo-ded@yandex.ru>
2023-08-10 19:32:40 +0500

    3

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import { useIsTelegramWebAppReady,  useTelegramWebApp} from "react-telegram-weba
 import React from "react";
 import clsx from "clsx";
 import {Header} from './Header/Header';
+import { TelegramWebApp } from "react-telegram-webapp";
 
 
 function App() {
     const isReady = useIsTelegramWebAppReady();
     const tgApp = useTelegramWebApp();
 
-
+    console.log('TelegramWebApp',TelegramWebApp)
     return (
         <div style={{color: isReady ? "green" : "black"}}>Work
         <pre
             style={{
+                border: "solid 1px #000",
                 display: "block",
                 wordWrap: "break-word",
                 width: "300px",
                 overflowWrap: "break-word"
             }}
         >
-          {JSON.stringify(tgApp)}
+          {JSON.stringify(TelegramWebApp)}
         </pre>
          {/*   <div>{tgApp.initDataUnsafe?.user?.id}</div>*/}
 
